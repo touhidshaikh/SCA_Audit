2a1483f28017caf099ad6272013ce02ccd7fc05a
Touhid M Shaikh <touhidshaikh22@gmail.com>
2023-01-24 11:44:08 -0500

    updated

--- npm_sbom.py
+++ npm_sbom.py
@@ -18,9 +18,15 @@ def generate_sbom(file_path):
                     license = license_info.stdout.strip()
                 except:
                     license = "Unknown"
+
+                # check for latest version available
+                try:
+                    latest_version = subprocess.run(["npm", "view", name, "version"], capture_output=True, text=True)
+                    latest_version = latest_version.stdout.strip()
+                except:
+                    latest_version = "Unknown"
                 # Append the dependency information to the SBOM
-                sbom.append({'name': name, 'version': version, 'license': license})
+                sbom.append({'name': name, 'version': version, 'latest_version':latest_version, 'dependency_type': dep_type, 'license': license})
 
     # Return the generated SBOM
     return sbom
-
